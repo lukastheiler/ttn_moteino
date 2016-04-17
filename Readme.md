accef00957ae0a8bb9f4f9458dac58eb87a828fe
Lukas Theiler <lukas.theiler@gmail.com>
2016-04-17 17:51:33 +0200

    readme

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# The Thing network - Arduino Monteino Lora walkthrough
+Step by step guide to get the Monteino Lora edition up and running.
+Shopping list:
+- https://lowpowerlab.com/shop/Moteino/moteinomega
+
+# Sodering
+Ouf ot the box, the Monteino is meant for peer to peer communication. In order to make the SX1276 chip talk to the things network, you'll have to soder at least the D0 and D1 pin. My sodering skills are far beyond Urs Marti's so I just show you his image:
+![monteinomega_for_lora-ttn_1024.jpg](/images/monteinomega_for_lora-ttn_1024.jpg)
---
+++ mqtt.js
@@ -0,0 +1,23 @@
+var mqtt = require('mqtt');
+
+var client = mqtt.connect('tcp://croft.thethings.girovito.nl:1883');
+// nodes/5A480881/packets
+// gateways/B827EBFFFEC7F595/status
+
+var i=0;
+
+client.on('connect', function () {
+    nodes=['nodes/5A480BED/packets','nodes/5A48DEAD/packets','nodes/5A48FEED/packets'];
+    nodes.forEach(n => {
+    	   console.log("connected, subscribed "+n);
+       	 client.subscribe(n);
+        })
+});
+
+client.on('message', function (topic, message) {
+  console.log("["+i+"] "+new Date()+" from "+topic.toString());
+  var msg = JSON.parse(message);
+  msg.data=new Buffer(msg.data, 'base64').toString('ascii')
+  console.log(msg);
+  ++i;
+});
