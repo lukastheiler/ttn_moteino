abc4c9f0ee974269f360ff2487600b0628788477
Lukas Theiler <lukas.theiler@gmail.com>
2016-04-21 14:37:54 +0200

    Fix: rename sketches

--- ttn_moteino/ttn_moteino.ino
+++ ttn_moteino/ttn_moteino.ino
@@ -37,7 +37,7 @@ static const u1_t PROGMEM APPSKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0x
 
 // LoRaWAN end-device address (DevAddr)
 // See http://thethingsnetwork.org/wiki/AddressSpace
-static const u4_t DEVADDR = 0xFEEDBEEF ; // <-- Change this address for every node!
+static const u4_t DEVADDR = 0xAFFE2719 ; // <-- Change this address for every node!
 
 // These callbacks are only used in over-the-air activation, so they are
 // left empty here (we cannot leave them out completely unless
